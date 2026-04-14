7495be6b5f6fef21cccf459d7e2171d86a825c93
s33611MS <s33611@pjwstk.edu.pl>
2026-04-14 22:17:53 +0200

    add Room and Reservation models

---
+++ APBD5/Enums/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace APBD5.Enums;
+
+public enum Status
+{
+    Planned, 
+    Confirmed, 
+    Cancelled
+}
---
+++ APBD5/Models/Room.cs
@@ -0,0 +1,12 @@
+﻿namespace APBD5.Models;
+
+public class Room
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int BuildingCode { get; set; }
+    public int Floor { get; set; }
+    public int Capacity { get; set; }
+    public bool HasProjector { get; set; }
+    public bool IsActive { get; set; }
+}
