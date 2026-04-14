c6e203b4a9eb45a6c897e1b02aff7e87e1e104f2
s33611MS <s33611@pjwstk.edu.pl>
2026-04-14 22:45:31 +0200

    add Rooms and Reservations controllers

---
+++ APBD5/Controllers/ReservationsController.cs
@@ -0,0 +1,24 @@
+﻿using APBD5.Enums;
+using APBD5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ReservationsController
+{
+    private static List<Reservation> _reservations = [
+        new Reservation()
+        {
+            Id = 1,
+            RoomId = 1,
+            OrganizerName = "Nowak",
+            Topic = "Interesting topic",
+            Date = new DateOnly(2026, 04, 16),
+            StartTime =  new DateTime(2026, 04, 16, 10,  30, 00),
+            EndTime =  new DateTime(2026, 04, 16, 12,  00, 00),
+            Status = Status.Planned
+        }
+    ];
+}
---
+++ APBD5/Controllers/RoomsController.cs
@@ -0,0 +1,23 @@
+﻿using APBD5.Enums;
+using APBD5.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD5.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController
+{
+    private static List<Room> _rooms = [
+        new Room()
+        {
+            Id = 1,
+            Name = "Room 1",
+            BuildingCode = 1234,
+            Capacity = 100,
+            Floor = 5,
+            HasProjector =  false,
+            IsActive = true
+        }
+    ];
+}
