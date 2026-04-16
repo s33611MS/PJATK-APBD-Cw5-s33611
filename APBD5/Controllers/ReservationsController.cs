16d6c3a47b54215ae5e6b19f49af523742e2d251
s33611MS <s33611@pjwstk.edu.pl>
2026-04-16 19:12:59 +0200

    add example data

--- APBD5/Controllers/ReservationsController.cs
+++ APBD5/Controllers/ReservationsController.cs
@@ -19,6 +19,61 @@ public class ReservationsController
             StartTime =  new DateTime(2026, 04, 16, 10,  30, 00),
             EndTime =  new DateTime(2026, 04, 16, 12,  00, 00),
             Status = Status.Planned
-        }
+        },
+        new Reservation()
+        {
+            Id = 2,
+            RoomId = 5,
+            OrganizerName = "Smith",
+            Topic = "Other topic",
+            Date = new DateOnly(2026, 05, 01),
+            StartTime =  new DateTime(2026, 05, 01, 11,  00, 00),
+            EndTime =  new DateTime(2026, 05, 01, 13,  00, 00),
+            Status = Status.Planned
+        },
+        new Reservation()
+        {
+            Id = 3,
+            RoomId = 3,
+            OrganizerName = "Pjatkowski",
+            Topic = "Definitely something",
+            Date = new DateOnly(2026, 03, 20),
+            StartTime =  new DateTime(2026, 03, 20, 16,  00, 00),
+            EndTime =  new DateTime(2026, 03, 20, 16,  30, 00),
+            Status = Status.Confirmed
+        },
+        new Reservation()
+        {
+            Id = 4,
+            RoomId = 3,
+            OrganizerName = "Nowak",
+            Topic = "Another interesting topic",
+            Date = new DateOnly(2026, 04, 23),
+            StartTime =  new DateTime(2026, 04, 23, 10,  30, 00),
+            EndTime =  new DateTime(2026, 04, 23, 12,  00, 00),
+            Status = Status.Planned
+        },
+        new Reservation()
+        {
+            Id = 5,
+            RoomId = 2,
+            OrganizerName = "Pjatkowski",
+            Topic = "Not interesting enough topic",
+            Date = new DateOnly(2025, 12, 06),
+            StartTime =  new DateTime(2025, 12, 06, 18,  30, 00),
+            EndTime =  new DateTime(2026, 12, 06, 20,  00, 00),
+            Status = Status.Cancelled
+        },
+        new Reservation()
+        {
+            Id = 6,
+            RoomId = 4,
+            OrganizerName = "Nowak",
+            Topic = "Yet another interesting topic",
+            Date = new DateOnly(2026, 06, 18),
+            StartTime =  new DateTime(2026, 06, 18, 12,  15, 00),
+            EndTime =  new DateTime(2026, 06, 18, 16,  45, 00),
+            Status = Status.Confirmed
+        },
     ];
 }
--- APBD5/Controllers/RoomsController.cs
+++ APBD5/Controllers/RoomsController.cs
@@ -18,6 +18,46 @@ public class RoomsController
             Floor = 5,
             HasProjector =  false,
             IsActive = true
-        }
+        },
+        new Room()
+        {
+            Id = 2,
+            Name = "Room 2",
+            BuildingCode = 2442,
+            Capacity = 15,
+            Floor = 4,
+            HasProjector =  false,
+            IsActive = false
+        },
+        new Room()
+        {
+            Id = 3,
+            Name = "Room 3",
+            BuildingCode = 3333,
+            Capacity = 50,
+            Floor = 3,
+            HasProjector =  true,
+            IsActive = true
+        },
+        new Room()
+        {
+            Id = 4,
+            Name = "Room 4",
+            BuildingCode = 4224,
+            Capacity = 20,
+            Floor = 2,
+            HasProjector =  true,
+            IsActive = false
+        },
+        new Room()
+        {
+            Id = 5,
+            Name = "Room 5",
+            BuildingCode = 5678,
+            Capacity = 80,
+            Floor = 3,
+            HasProjector =  true,
+            IsActive = true
+        },
     ];
 }
